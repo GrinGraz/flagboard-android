61fbbac04bd00a04652d43810ac05460f5cf9cde
Christopher Ruz <c.ruz.olate@gmail.com>
2022-04-18 09:34:33 -0300

    Initial commit

---
+++ app/src/main/java/cl/gringraz/flagboard/MainActivity.kt
@@ -0,0 +1,48 @@
+package cl.gringraz.flagboard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import cl.gringraz.flagboard.ui.theme.FlagboardTheme
+import cl.gringraz.flagboard_android.FlagBoard
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlagboardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val context = LocalContext.current
+    FlagBoard.init(mapOf("Boolean flag" to true, "String flag" to "hello", "Int flag"
+            to 1, "Json flag" to "{\"key\":\"value\"}"))
+    Text(text = "Hello $name", modifier = Modifier.clickable {
+        FlagBoard.open(context)
+    })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FlagboardTheme {
+        Greeting("Android")
+    }
+}
---
+++ flagboard-android/src/main/java/cl/gringraz/flagboard_android/FlagBoard.kt
@@ -0,0 +1,84 @@
+package cl.gringraz.flagboard_android
+
+import android.content.Context
+import androidx.annotation.NonNull
+import org.json.JSONObject
+
+
+object FlagBoard {
+    @JvmInline
+    internal value class Key(val value: String)
+    internal data class Param<T>(val key: Key, val value: T)
+
+    sealed interface FeatureFlag
+    @JvmInline
+    internal value class IntFlag(val param: Param<Int>) : FeatureFlag
+    @JvmInline
+    internal value class JsonFlag(val param: Param<JSONObject>) : FeatureFlag
+    @JvmInline
+    internal value class StringFlag(val param: Param<String>) : FeatureFlag
+    @JvmInline
+    internal value class BooleanFlag(val param: Param<Boolean>) : FeatureFlag
+    @JvmInline
+    internal value class UnknownFlag(val type: Any) : FeatureFlag
+
+    internal var flagBoardState: FlagBoardState = FlagBoardState.NOT_INITIALIZED
+
+    internal var featureFlags: List<FeatureFlag> = emptyList()
+    internal var thisFeatureFlagsMap: Map<String, Any> = emptyMap()
+
+    fun init(@NonNull featureFlagsMap: Map<String, Any>): FlagBoard? {
+        if (BuildConfig.DEBUG) {
+            if (flagBoardState == FlagBoardState.INITIALIZED) {
+                println("FlagBoard was already initialized. Previous parameters will be " +
+                        "overwritten")
+            }
+            flagBoardState = FlagBoardState.INITIALIZED
+            thisFeatureFlagsMap = featureFlagsMap
+            featureFlags = parseToFeatureFlags(featureFlagsMap)
+            println("FlagBoard is initialized")
+            return this
+        }
+        return null
+    }
+
+    fun open(@NonNull context: Context) {
+        if (BuildConfig.DEBUG) {
+            when(flagBoardState) {
+                FlagBoardState.INITIALIZED -> {
+                    FlagBoardActivity.openFlagBoard(context)
+                }
+                FlagBoardState.NOT_INITIALIZED -> println("FlagBoard is not initialized. Before opening" +
+                        " call the FlagBoard.init(@NonNull featureFlagsMap: Map<String, Any>) function")
+                FlagBoardState.UNKNOWN -> println("FlagBoard is a inconsistent state. Recall the " +
+                        "FlagBoard.init(@NonNull featureFlagsMap: Map<String, Any>) function")
+            }
+        }
+    }
+
+    private fun parseToFeatureFlags(featureFlagsMap: Map<String, Any>): List<FeatureFlag> =
+        featureFlagsMap.map { entry ->
+            when (val value = entry.value) {
+                is Int -> IntFlag(Param(key = Key(value = entry.key), value = value))
+                is String -> getStringType(Param(key = Key(value = entry.key), value = value))
+                is Boolean -> BooleanFlag(Param(key = Key(value = entry.key), value = value))
+                else -> UnknownFlag(entry.value)
+            }
+        }
+
+    private fun getStringType(param: Param<String>): FeatureFlag = when {
+        param.value.contains("{") -> JsonFlag(
+            param = Param(
+                key = param.key,
+                value = JSONObject(param.value)
+            )
+        )
+        else -> StringFlag(param = param)
+    }
+
+    internal enum class FlagBoardState {
+        INITIALIZED,
+        NOT_INITIALIZED,
+        UNKNOWN,
+    }
+}
