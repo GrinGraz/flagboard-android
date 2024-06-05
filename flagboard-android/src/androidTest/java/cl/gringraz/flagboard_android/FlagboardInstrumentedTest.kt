6f9382d2a5949fc81eb06f410b70c4859ddc795b
Christopher William Ruz Olate <christopher.ruz@bci.cl>
2024-06-05 12:28:05 -0400

    Add test for Double value behavior

--- flagboard-android/src/androidTest/java/cl/gringraz/flagboard_android/FlagboardInstrumentedTest.kt
+++ flagboard-android/src/androidTest/java/cl/gringraz/flagboard_android/FlagboardInstrumentedTest.kt
@@ -24,9 +24,11 @@ class FlagboardInstrumentedTest {
     private val intFlagMap = mapOf("one key" to 1, "two key" to 2)
     private val longFlagMap = mapOf("one key" to 1L, "two key" to 2L)
     private val jsonFlagMap = mapOf("json key" to "{\"key\":\"value\"}")
+    private val doubleFlagMap = mapOf("one key" to 1.0, "two key" to 2.0)
 
     @Before
     fun reset() {
+        Flagboard.init(context = context)
         Flagboard.reset()
     }
 
@@ -95,6 +97,16 @@ class FlagboardInstrumentedTest {
     @Test
     fun getFlagboardWrongLongKeyFlagShouldReturnMinusOne() {
         assertEquals(-1, Flagboard.loadFlags(longFlagMap).getLong(wrongKey))
+    }
+
+    @Test
+    fun getFlagboardDoubleFlagShouldReturnDouble() {
+        assertEquals(1.0, Flagboard.loadFlags(doubleFlagMap).getDouble("one key"), 0.0)
+    }
+
+    @Test
+    fun getFlagboardWrongDoubleKeyFlagShouldReturnMinusOne() {
+        assertEquals(-1.0, Flagboard.loadFlags(doubleFlagMap).getDouble(wrongKey), 0.0)
     }
 
     @Test
